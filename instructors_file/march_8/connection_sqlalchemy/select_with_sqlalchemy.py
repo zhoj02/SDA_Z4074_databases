8fb9b5276e20d44ef168401a8154a2eb53885c1b
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-08 11:00:34 +0100

    Mar 8 - select with sql alchemy

---
+++ instructors_file/march_8/connection_sqlalchemy/select_with_sqlalchemy.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost:3306/mysql')
+
+Base = declarative_base()
+Session = sessionmaker(bind=db)
+
+session = Session()
+result = session.execute(text("SHOW TABLES;"))
+for table in result:
+    print(table)
