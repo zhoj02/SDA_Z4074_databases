72bc00978bd5fc6425e53c6cf7f7458ea73620a8
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-08 10:48:01 +0100

    Mar 8 - sql alchmey connect

---
+++ instructors_file/march_8/connection_sqlalchemy/connection_test.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost:3306/mysql')
+
+Base = declarative_base()
+Session = sessionmaker(bind=db)
+
+session = Session()
