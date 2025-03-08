a423a252b001ae78963de15df1703660df63d68f
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-08 10:51:24 +0100

    Mar 8 - sql alchemy - removed warning

--- instructors_file/march_8/connection_sqlalchemy/connection_test.py
+++ instructors_file/march_8/connection_sqlalchemy/connection_test.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost:3306/mysql')
