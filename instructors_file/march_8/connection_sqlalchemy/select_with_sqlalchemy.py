f0ba477b020feabba8a0fd295da8428bc85b9164
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-08 11:15:14 +0100

    Mar 8 - select with sql alchemy

--- instructors_file/march_8/connection_sqlalchemy/select_with_sqlalchemy.py
+++ instructors_file/march_8/connection_sqlalchemy/select_with_sqlalchemy.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost:3306/mysql')
+db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost/car_rental')
 
 Base = declarative_base()
 Session = sessionmaker(bind=db)
