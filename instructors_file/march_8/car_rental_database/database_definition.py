8965a75acac5ca4110f6f99f5be9f418ebf9128c
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-08 13:56:48 +0100

    Mar 8 - database definition

---
+++ instructors_file/march_8/car_rental_database/database_definition.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Integer, Date
+
+Base = declarative_base()
+
+class Cars(Base):
+    __tablename__ = 'cars'
+
+    car_id = Column(Integer, primary_key=True, autoincrement=True)
+    producer = Column(String(30), nullable=False)
+    model = Column(String(30), nullable=False)
+    year = Column(Integer, nullable=False)
+    horse_power = Column(Integer, nullable=False)
+    price_per_day = Column(Integer, nullable=False)
+
+
+class Clients(Base):
+    __tablename__ = 'clients'
+
+    client_id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(30), nullable=False)
+    surname = Column(String(30), nullable=False)
+    address = Column(String(30), nullable=False)
+    city = Column(String(30), nullable=False)
+
+
+class Bookings(Base):
+    __tablename__ = 'bookings'
+
+    booking_id = Column(Integer, primary_key=True, autoincrement=True)
+    client_id = Column(Integer, nullable=False)
+    car_id = Column(Integer, nullable=False)
+    start_date = Column(Date, nullable=False)
+    end_date = Column(Date, nullable=False)
+    total_amount = Column(Integer, nullable=False)
