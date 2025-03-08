0f0968073404626a67927da48decbc34b276c569
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-08 10:00:16 +0100

    Mar 8 - test connect -2

--- instructors_file/connect_test.py
+++ instructors_file/connect_test.py
@@ -5,3 +5,4 @@ test_select = "SELECT 1;"
 with connect(user='root', password='YourNewPassword') as conn:
     with conn.cursor() as cursor:
         cursor.execute(test_select)
+        print(cursor.fetchall())
