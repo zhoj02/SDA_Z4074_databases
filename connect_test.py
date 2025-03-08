8f18b5e5c74840a53c0146ec695c718c03c8ab7f
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-08 09:56:39 +0100

    Mar 8 - test connect

---
+++ connect_test.py
@@ -0,0 +1,7 @@
+from mysql.connector import connect
+
+test_select = "SELECT 1;"
+
+with connect(user='root', password='YourNewPassword') as conn:
+    with conn.cursor() as cursor:
+        cursor.execute(test_select)
