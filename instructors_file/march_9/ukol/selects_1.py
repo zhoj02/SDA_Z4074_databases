c67222467628caab9f40ef126593e4bd13bfe3db
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 13:20:55 +0100

    March 9 - ukol banka - selecty 1

---
+++ instructors_file/march_9/ukol/selects_1.py
@@ -0,0 +1,6 @@
+# Ukoly
+
+# Ukol c.1 - Vyber transakce, které jsou nižší než ... (vyber podle tvých transakcí)
+# Ukol c.2 - Vyber transakce, které jsou vyšší než ... (vyber podle tvých transakcí)
+# Ukol c.3 - Vyber transakce, které jsou starší než ... (vyber podle tvých transakcí)
+# Ukol c.4 - Vyber transakce, které jsou starší než ... a mají vyšší hodnotu než ...(vyber podle tvých transakcí)
