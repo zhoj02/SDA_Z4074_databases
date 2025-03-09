e6f03af1880e1381c2343f2af4d448d40f4051b3
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 13:17:58 +0100

    March 9 - ukol banka - create table

--- instructors_file/march_9/ukol/bank_database.py
+++ instructors_file/march_9/ukol/bank_database.py
@@ -7,3 +7,16 @@ Bude mi 2 tabulky:
 Max 4 sloupce do tabulky, necham na vas ty sloupce.
  Ale v transakci musi byt datum a castka transakce.
 """
+
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+with open("moje_heslo.txt", 'r') as file:
+    password = file.read()
+
+db = create_engine(f'mysql+mysqlconnector://root:{password}@localhost:3306/')
+
+Session = sessionmaker(bind=db)
+
+session = Session()
+session.execute(text("CREATE DATABASE bank"))
