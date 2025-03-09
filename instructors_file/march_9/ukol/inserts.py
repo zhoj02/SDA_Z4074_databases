f96a5da59f7adc650b5ac530d31ee474bed49bbd
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 13:46:40 +0100

    March 9 - inserts 5

--- instructors_file/march_9/ukol/inserts.py
+++ instructors_file/march_9/ukol/inserts.py
@@ -16,8 +16,25 @@ clients = [
     {"name": "Petr", "surname": "Malý", "city": "Bratislava"},
 ]
 
+transactions = [
+    {"date": "2022-07-10 14:00:00", "amount": 1},
+    {"date": "2022-07-11 13:00:00", "amount": 200},
+    {"date": "2022-08-10 18:00:00", "amount": 200},
+    {"date": "2022-09-10 14:00:00", "amount": 500},
+    {"date": "2022-07-10 14:00:00", "amount": 2800},
+    {"date": "2022-10-10 19:00:00", "amount": 20550},
+    {"date": "2022-07-10 14:00:00", "amount": 200},
+    {"date": "2022-10-10 17:00:00", "amount": 200},
+    {"date": "2022-10-10 14:00:00", "amount": 547},
+    {"date": "2022-10-11 14:00:00", "amount": 200},
+]
+
 for client in clients:
     session.add(Clients(**client))
+    session.commit()
+
+for transaction in transactions:
+    session.add(Transactions(**transaction))
     session.commit()
 
 
