8f1a227423369788b3b60a428f46c3071144f412
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 13:31:18 +0100

    March 9 - database definition

---
+++ instructors_file/march_9/ukol/database_definition_bank.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Integer, Time
+
+with open("moje_heslo.txt", 'r') as file:
+    password = file.read()
+
+db = create_engine(f'mysql+mysqlconnector://root:{password}@localhost:3306/bank')
+
+Base = declarative_base()
+
+
+class Transactions(Base):
+    __tablename__ = 'transactions'
+
+    car_id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(Time, nullable=False)
+    amount = Column(Integer, nullable=False)
+
+
+class Clients(Base):
+    __tablename__ = 'clients'
+
+    client_id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(30), nullable=False)
+    surname = Column(String(30), nullable=False)
+    city = Column(String(30), nullable=False)
--- instructors_file/march_9/ukol/inserts.py
+++ instructors_file/march_9/ukol/inserts.py
@@ -3,3 +3,4 @@
 # Ukol c.1 - Vloz do database 3 klienty
 
 # Ukol c.2 - Vloz do database 10 transakci
+
