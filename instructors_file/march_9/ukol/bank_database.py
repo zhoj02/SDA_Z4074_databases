7b93e3469477eead2c0af0ebc8f1e4324a70ce1a
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 12:58:26 +0100

    March 9 - Ukol banka

---
+++ instructors_file/march_9/ukol/bank_database.py
@@ -0,0 +1,9 @@
+"""
+Vytvorte databazi bank.
+Bude mi 2 tabulky:
+1. Transakce
+2. Zakaznik
+
+Max 4 sloupce do tabulky, necham na vas ty sloupce.
+ Ale v transakci musi byt datum a castka transakce.
+"""
