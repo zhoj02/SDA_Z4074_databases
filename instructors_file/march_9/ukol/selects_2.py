18231e239f153eb0dc3ae031a6dd47c9f9a473f2
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 14:20:11 +0100

    March 9 - selects 4

--- instructors_file/march_9/ukol/selects_2.py
+++ instructors_file/march_9/ukol/selects_2.py
@@ -4,3 +4,18 @@
 # Ukol c.6 - Vyber transakce, které mají hodnotu nižší než průměr vašich transakcí
 # Ukol c.7 - Vyber transakce, které mají hodnotu nižší než 30. quantil vašich transakcí
 # Ukol c.8 - Vyber zákazníka, jehož transakce proběhla jako poslední
+
+from sqlalchemy.orm import sessionmaker
+
+from database_definition_bank import Transactions, db, Clients
+
+Session = sessionmaker(bind=db)
+session = Session()
+
+all_transactions = session.query(Transactions.amount).all()
+print(all_transactions)
+amounts = []
+for amount in all_transactions:
+    amounts.append(amount[0])
+print(amounts)
+
