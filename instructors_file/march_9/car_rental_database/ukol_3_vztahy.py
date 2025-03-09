02abd656378b442d6d9f4a39ed8a010abb7dc2ef
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 09:19:12 +0100

    March 9 - Ukol 3 - start 2

--- instructors_file/march_9/car_rental_database/ukol_3_vztahy.py
+++ instructors_file/march_9/car_rental_database/ukol_3_vztahy.py
@@ -1,3 +1,6 @@
 from sqlalchemy.orm import sessionmaker
 
 from instructors_file.march_8.car_rental_database.database_definition import Cars, Bookings, Clients, db
+
+Session = sessionmaker(bind=db)
+session = Session()
