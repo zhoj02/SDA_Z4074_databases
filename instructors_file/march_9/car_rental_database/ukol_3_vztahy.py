05ce6498b018700bbce968ee708cbed3a02f4111
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 09:30:48 +0100

    March 9 - Ukol 3 - start 3

--- instructors_file/march_9/car_rental_database/ukol_3_vztahy.py
+++ instructors_file/march_9/car_rental_database/ukol_3_vztahy.py
@@ -1,6 +1,20 @@
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
 
 from instructors_file.march_8.car_rental_database.database_definition import Cars, Bookings, Clients, db
 
 Session = sessionmaker(bind=db)
 session = Session()
+
+#1
+result = session.query(Bookings)
+for booking in result:
+    print(booking)
+
+#2
+select = select(Bookings)
+conn = db.connect()
+result = conn.execute(select).fetchall()
+print(result)
+
+
