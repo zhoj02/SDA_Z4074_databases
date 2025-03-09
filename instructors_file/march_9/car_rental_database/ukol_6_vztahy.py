9ab8e32894961d992de12709950229b46fc006a3
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 12:22:38 +0100

    March 9 - Ukol 6

---
+++ instructors_file/march_9/car_rental_database/ukol_6_vztahy.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
+from sqlalchemy import and_
+
+from instructors_file.march_8.car_rental_database.database_definition import Cars, Bookings, Clients, db
+
+Session = sessionmaker(bind=db)
+session = Session()
