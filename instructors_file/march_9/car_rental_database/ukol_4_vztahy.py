7a134a0a1aea6208005e1dd4a2a5157885259159
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 10:06:27 +0100

    March 9 - Ukokl 4 pokracovani

--- instructors_file/march_9/car_rental_database/ukol_4_vztahy.py
+++ instructors_file/march_9/car_rental_database/ukol_4_vztahy.py
@@ -5,3 +5,11 @@ from instructors_file.march_8.car_rental_database.database_definition import Car
 
 Session = sessionmaker(bind=db)
 session = Session()
+
+#2
+result = session.query(Bookings).filter_by(client_id=5)
+# result = session.query(Bookings).filter(Bookings.client_id==5)
+for booking in result:
+    print(booking.car)
+
+#1
