a8b8e2a2c6ae3f6bc8eb31ef0ded644ed61230af
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 11:07:11 +0100

    March 9 - Ukol 6

--- instructors_file/march_9/car_rental_database/ukol_5_vztahy.py
+++ instructors_file/march_9/car_rental_database/ukol_5_vztahy.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import select
 from sqlalchemy import and_
@@ -8,9 +9,7 @@ Session = sessionmaker(bind=db)
 session = Session()
 
 bookings = session.query(Clients.client_id, Clients.name, Bookings.total_amount).join(Clients).filter(
-    and_(Bookings.total_amount>1100, Bookings.start_date >='2020-07-14'))
-for booking in bookings:
-    print(booking)
+    and_(Bookings.total_amount > 1100, Bookings.start_date >= '2020-07-14'))
 
 """
 Id, jmeno zakaznika, castku za pujceni
