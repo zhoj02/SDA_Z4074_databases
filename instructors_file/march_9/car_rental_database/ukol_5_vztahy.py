27fc5267e28fd9419d91eb757582549e0b9ca1f0
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 10:48:54 +0100

    March 9 - Ukol 5 - pokracovani

--- instructors_file/march_9/car_rental_database/ukol_5_vztahy.py
+++ instructors_file/march_9/car_rental_database/ukol_5_vztahy.py
@@ -1,12 +1,14 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import select
+from sqlalchemy import and_
 
 from instructors_file.march_8.car_rental_database.database_definition import Cars, Bookings, Clients, db
 
 Session = sessionmaker(bind=db)
 session = Session()
 
-bookings = session.query(Bookings)
+bookings = session.query(Clients.client_id, Clients.name, Bookings.total_amount).join(Clients).filter(
+    and_(Bookings.total_amount>1100, Bookings.start_date >='2020-07-14'))
 for booking in bookings:
     print(booking)
 
