1342fed290cb3c8dcb0064eea83c969fc7c798d9
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 13:27:06 +0100

    March 9 - ukol banka - all create tables

--- instructors_file/march_9/ukol/create_table.py
+++ instructors_file/march_9/ukol/create_table.py
@@ -1,24 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, String, Integer, Date
+from sqlalchemy import Column, String, Integer, Time
 
 with open("moje_heslo.txt", 'r') as file:
     password = file.read()
 
-db = create_engine(f'mysql+mysqlconnector://root:{password}@localhost:3306/car_rental')
+db = create_engine(f'mysql+mysqlconnector://root:{password}@localhost:3306/bank')
 
 Base = declarative_base()
 
 
-class Cars(Base):
-    __tablename__ = 'cars'
+class Transactions(Base):
+    __tablename__ = 'transactions'
 
     car_id = Column(Integer, primary_key=True, autoincrement=True)
-    producer = Column(String(30), nullable=False)
-    model = Column(String(30), nullable=False)
-    year = Column(Integer, nullable=False)
-    horse_power = Column(Integer, nullable=False)
-    price_per_day = Column(Integer, nullable=False)
+    date = Column(Time, nullable=False)
+    amount = Column(Integer, nullable=False)
 
 
 class Clients(Base):
@@ -27,19 +24,7 @@ class Clients(Base):
     client_id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(30), nullable=False)
     surname = Column(String(30), nullable=False)
-    address = Column(String(30), nullable=False)
     city = Column(String(30), nullable=False)
-
-
-class Bookings(Base):
-    __tablename__ = 'bookings'
-
-    booking_id = Column(Integer, primary_key=True, autoincrement=True)
-    client_id = Column(Integer, nullable=False)
-    car_id = Column(Integer, nullable=False)
-    start_date = Column(Date, nullable=False)
-    end_date = Column(Date, nullable=False)
-    total_amount = Column(Integer, nullable=False)
 
 
 Base.metadata.create_all(db)
