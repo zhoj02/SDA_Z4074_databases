46b48e26f98e27d5d2cdfd7817d38e5c5b4705c6
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 13:39:54 +0100

    March 9 - inserts 4

--- instructors_file/march_9/ukol/inserts.py
+++ instructors_file/march_9/ukol/inserts.py
@@ -1,4 +1,5 @@
 # Ukoly
+from sqlalchemy.orm import sessionmaker
 
 # Ukol c.1 - Vloz do database 3 klienty
 
@@ -6,9 +7,17 @@
 
 from database_definition_bank import Transactions, db, Clients
 
+Session = sessionmaker(bind=db)
+session = Session()
+
 clients = [
     {"name": "Jan", "surname": "Zhouf", "city": "Prague"},
     {"name": "Jan", "surname": "Marek", "city": "Kolín"},
     {"name": "Petr", "surname": "Malý", "city": "Bratislava"},
 ]
+
+for client in clients:
+    session.add(Clients(**client))
+    session.commit()
+
 
