9d41af164f9ebb2dd08f3afed646089ba3e882f9
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-09 13:49:29 +0100

    March 9 - selects 2

---
+++ instructors_file/march_9/ukol/selects_2.py
@@ -0,0 +1,6 @@
+# Ukoly
+
+# Ukol c.5 - Vyber transakce, které mají hodnotu nižší než medián vašich transakcí
+# Ukol c.6 - Vyber transakce, které mají hodnotu nižší než průměr vašich transakcí
+# Ukol c.7 - Vyber transakce, které mají hodnotu nižší než 30. quantil vašich transakcí
+# Ukol c.8 - Vyber zákazníka, jehož transakce proběhla jako poslední
