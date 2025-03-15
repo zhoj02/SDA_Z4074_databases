10b57e317ee9a3069ee272903aae59c1b83cae78
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-15 11:03:55 +0100

    March 15

---
+++ instructors_file/opakovani/delete_data.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, String, Integer, Time
+
+with open("moje_heslo.txt", 'r') as file:
+    password = file.read()
+
+db = create_engine(f'mysql+mysqlconnector://root:{password}@localhost:3306/hobby_market')
+
+Base = declarative_base()
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    nazev_polozky = Column(String(20))
+
+    def __repr__(self):
+        return self.nazev_polozky
+
+
+session = sessionmaker(db)()
+
+print(session.query(Item).filter(Item.id==1).first())
+print(session.query(Item).filter_by(id=1).first())
