26621d2ca7dafc32fb383009bf4cb9252a78c21c
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-15 09:59:41 +0100

    March 15

--- instructors_file/opakovani/sql_alchemy_opakovani.py
+++ instructors_file/opakovani/sql_alchemy_opakovani.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import Column, String, Integer, Time
 
 with open("moje_heslo.txt", 'r') as file:
@@ -9,3 +9,7 @@ db = create_engine(f'mysql+mysqlconnector://root:{password}@localhost:3306/bank'
 
 Base = declarative_base()
 select_test = "SELECT 1;"
+Session = sessionmaker(db)
+session = Session()
+
+print(session.execute(text(select_test)).all())
