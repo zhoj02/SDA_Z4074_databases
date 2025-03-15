73317c2df3ee25a14c7f606afec84c595ab53104
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-15 09:51:12 +0100

    March 15

---
+++ instructors_file/opakovani/sql_alchemy_opakovani.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Integer, Time
+
+with open("moje_heslo.txt", 'r') as file:
+    password = file.read()
+
+db = create_engine(f'mysql+mysqlconnector://root:{password}@localhost:3306/bank')
+
+Base = declarative_base()
