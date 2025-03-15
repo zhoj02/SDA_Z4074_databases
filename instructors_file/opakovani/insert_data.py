136fafd164ca0cf63b396df7be9b74f940189706
jan.zhouf <jan.zhouf@resulmatic.com>
2025-03-15 10:34:18 +0100

    March 15

---
+++ instructors_file/opakovani/insert_data.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, String, Integer, Time
+
+with open("moje_heslo.txt", 'r') as file:
+    password = file.read()
+
+db = create_engine(f'mysql+mysqlconnector://root:{password}@localhost:3306/hobby_market')
+
+Base = declarative_base()
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    nazev_polozky = Column(String(20))
+
